3fc18b253605b6f42784cb4a1ccf4d1ad0b0b254
Juan José Valencia Jaramillo <juanchouni@hotmail.com>
2024-10-25 10:18:18 -0500

    MicroProyecto1 - Cluster Consul + Balanceador de carga usando HAProxy + Artillery en ambiente Vagrant

---
+++ microproyecto1/script.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+
+# Instalar Consult
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list 
+sudo apt install consul
+
+# Instalar node.js
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Asegurar version 18 nodejs
+nvm install 18
+nvm use 18
